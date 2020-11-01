ed59828cbe84430ff928a51f9fa04366aa25e76a
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-11-01 21:48:35 +0900

    added router query

--- src/About.js
+++ src/About.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import qs from 'qs';
 
-const About = () => {
+const About = ({ location }) => {
+    const query = qs.parse(location.search, {
+        ignoreQueryPrefix: true
+    });
+    const detail = query.detail === 'true'; // 쿼리의 파싱결과값은 문자열
     return (
         <div>
             <h1>소개</h1>
             <p>이 프로젝트는 이랙트 라우터 기초를 실습해보는 예제 프로젝트입니다.</p>
+            {detail && <p>추가적인 정보를 요청합니다.</p>}
         </div>
     )
 }
