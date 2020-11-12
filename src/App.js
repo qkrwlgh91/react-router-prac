d19b180bbf53c3d019025fd8fef244a58a574942
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-11-12 17:44:27 +0900

    useRef param management

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Profiles from './Profiles';
 import HistorySample from './HistorySample';
 import UseRefSample from './UseRefSample';
+import UserList from './UserList';
+
+// useRef 2번째 기능
+/*
+  컴포넌트 안에서 조회 및 수정 할 수 있는 변수를 관리하는 것
+  useRef로 관리하는 변수는 값이 바뀐다고 해서 컴포넌트가 리렌더링되지 않는다.
+  리액트 컴포넌트에서의 상태는 상태를 바꾸는 함수를 호출하고 나서 그 다음 렌더링 이후로 업데이트 된 상태를 조회할 수 있는 반면,
+  useRef로 관리하고 있는 변수는 설정 후 바로 조회할 수 있음.
+  useRef로 관리 할수 있는 값
+  setTimeout, setInterval
+  scroll 위치
+  외부 라이브러리를 사용하여 생성된 인스턴스
+*/
 
 const App = () => {
+  const users = [
+    {
+      id: 1,
+      username: 'pjh111',
+      email: 'qkrw@sf.com'
+    },
+    {
+      id: 2,
+      username: 'asg32',
+      email: '32ags@sf.com'
+    },
+    {
+      id: 3,
+      username: 'arb34',
+      email: 'qsfaw@sf.com'
+    },
+  ]
+
+  const nextId = useRef(4);
+
+  // useRef()를 사용할 때 파라미터를 넣어주면, 이 값이 .current 값의 기본값이 됨
+  // 값 수정시에는 .current 값을 수정하면 됨
+  // 조회할 때는 .current 값을 조회
+
+  const onCreate = () => {
+    
+    nextId.current += 1;
+  }
+
   return (
     <div>
       <ul>
@@ -43,6 +85,7 @@ const App = () => {
           )}
         />
       </Switch>
+      <UserList users={users} />
     </div>
   )
 }
